8e1660943d7c89f1ca3fd17f7cd1558310a90988
Vladimir Bondar <vladi.bondar@gmail.com>
2021-11-09 15:47:33 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { Container } from './components/Container';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <Container></Container>;
+  }
+}
+
+export default App;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,7 @@
+import s from './Container.module.scss';
+
+const Container = ({ children }) => (
+  <div className={s.container}>{children}</div>
+);
+
+export { Container };
